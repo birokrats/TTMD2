51007ea3321c7f789b917eb46766c7fe7eafcda6
birokrats <nordy911@inbox.lv>
2013-12-09 18:08:59 +0200

    k

--- fuel/app/classes/model/orm/comment.php
+++ fuel/app/classes/model/orm/comment.php
@@ -6,6 +6,15 @@
  */
 class Model_Orm_Comment extends Orm\Model {
     //put your code here
+    protected static $_table_name = 'comments';
+    protected static $_primary_key = array('id');
+    protected static $_properties = array(
+        'id',
+        'comment',
+        'event_id',
+        'user_id',
+        'created'     
+    );
     
     
 }
